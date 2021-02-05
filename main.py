77cb588f0936b192b99cfcbe2cf2c087772a0c91
ThanThoai <Darkrai17071998@gmail.com>
2021-02-05 17:08:47 +0700

    .

--- main.py
+++ main.py
@@ -150,8 +150,8 @@ if __name__ == '__main__':
     )
 
     config.dataset = 'rwf-2000'
-    config.train_batch = 32
-    config.val_batch = 32
+    config.train_batch = 16
+    config.val_batch = 16
     config.learning_rate = 1e-2
     config.num_cv = 1
     main(config)
