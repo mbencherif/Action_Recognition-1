48864f63ec13ab5fb60a560e27071604680ce27c
ThanThoai <Darkrai17071998@gmail.com>
2021-02-06 00:55:27 +0700

    .

--- model.py
+++ model.py
@@ -15,12 +15,12 @@ def VioNet_C3D(config):
     model = C3D(num_classes=2).to(device)
 
     state_dict = torch.load('weights/C3D_Kinetics.pth')
-    for state in state_dict.keys():
-        print(state)
-    print("-------------------")
-    for param in model.named_modules():
-        print(param)
-    model.load_state_dict(state_dict)
+    # for state in state_dict.keys():
+    #     print(state)
+    # print("-------------------")
+    # for param in model.named_modules():
+    #     print(param)
+    model.load_state_dict(state_dict, strict = False)
     params = model.parameters()
 
     return model, params
