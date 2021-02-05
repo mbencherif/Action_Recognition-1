d9fe309ea87fbf478f08d5557fb541949c16ee51
ThanThoai <Darkrai17071998@gmail.com>
2021-02-05 17:58:40 +0700

    .

--- epoch.py
+++ epoch.py
@@ -16,21 +16,18 @@ def train(
     model.train()
 
     end_time = time.time()
-    targets_onehot = torch.FloatTensor(16, 2)
     for i, (inputs, targets) in enumerate(data_loader):
         inputs  = inputs.to(device)
         targets = targets.to(device)
         data_time.update(time.time() - end_time)
-        targets_onehot.zero()
-        targets_onehot.scatter_(1, targets, 1)
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward
         outputs = model(inputs)
         # print(outputs, targets)
-        loss = criterion(outputs, targets_onehot)
-        acc = calculate_accuracy(outputs, targets_onehot)
+        loss = criterion(outputs, targets)
+        acc = calculate_accuracy(outputs, targets)
 
         # meter
         losses.update(loss.item(), inputs.size(0))
